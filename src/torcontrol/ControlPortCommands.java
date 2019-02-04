c185659f404b57f5df6556719088a748771c5cd6
Julius Paffrath <juliuspaffrath@web.de>
2019-02-04 16:52:22 +0100

    Add Control commands for SIGNAL

--- src/torcontrol/ControlPortCommands.java
+++ src/torcontrol/ControlPortCommands.java
@@ -10,4 +10,16 @@ public final class ControlPortCommands {
 	public static final String AUTHENTICATE = "AUTHENTICATE";
 	public static final String GETINFO_VERSION = "GETINFO version";
 	public static final String QUIT = "QUIT";
+	
+	/* Define control commands 3.7 SIGNAL */
+	public static final String SIGNAL_RELOAD        = "SIGNAL RELOAD";
+	public static final String SIGNAL_SHUTDOWN      = "SIGNAL SHUTDOWN";
+	public static final String SIGNAL_DUMP          = "SIGNAL DUMP";
+	public static final String SIGNAL_DEBUG         = "SIGNAL DEBUG";
+	public static final String SIGNAL_HALT          = "SIGNAL HALT";
+	public static final String SIGNAL_CLEARDNSCACHE = "SIGNAL CLEARDNSCACHE";
+	public static final String SIGNAL_NEWNYM        = "SIGNAL NEWNYM";
+	public static final String SIGNAL_HEARTBEAT     = "SIGNAL HEARTBEAT";
+	public static final String SIGNAL_DORMANT       = "SIGNAL DORMANT";
+	public static final String SIGNAL_ACTIVE        = "SIGNAL ACTIVE";
 }
