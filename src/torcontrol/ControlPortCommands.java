9f1db224fd37e3b12f6b1042bb084f119a285d85
Julius Paffrath <juliuspaffrath@web.de>
2019-02-04 20:59:10 +0100

    Add GETINFO control commands

--- src/torcontrol/ControlPortCommands.java
+++ src/torcontrol/ControlPortCommands.java
@@ -8,7 +8,6 @@ package torcontrol;
  */
 public final class ControlPortCommands {
 	public static final String AUTHENTICATE = "AUTHENTICATE";
-	public static final String GETINFO_VERSION = "GETINFO version";
 	public static final String QUIT = "QUIT";
 	
 	/* Define control commands 3.7 SIGNAL */
@@ -22,4 +21,16 @@ public final class ControlPortCommands {
 	public static final String SIGNAL_HEARTBEAT     = "SIGNAL HEARTBEAT";
 	public static final String SIGNAL_DORMANT       = "SIGNAL DORMANT";
 	public static final String SIGNAL_ACTIVE        = "SIGNAL ACTIVE";
+	
+	/* Define control commands 3.9 GETINFO */
+	public static final String GETINFO_VERSION                            = "GETINFO version";
+	public static final String GETINFO_CONFIG_FILE                        = "GETINFO config-file";
+	public static final String GETINFO_CONFIG_DEFAULTS_FILE               = "GETINFO config-defaults-file";
+	public static final String GETINFO_CONFIG_TEXT                        = "GETINFO config-text";
+	public static final String GETINFO_EXIT_POLICY_DEFAULT                = "GETINFO exit-policy/default";
+	public static final String GETINFO_EXIT_POLICY_REJECT_PRIVATE_DEFAULT = "GETINFO exit-policy/reject-private/default";
+	public static final String GETINFO_EXIT_POLICY_REJECT_PRIVATE_RELAY   = "GETINFO exit-policy/reject-private/relay";
+	public static final String GETINFO_EXIT_POLICY_IPV4                   = "GETINFO exit-policy/ipv4";
+	public static final String GETINFO_EXIT_POLICY_IPV6                   = "GETINFO exit-policy/ipv6";
+	public static final String GETINFO_EXIT_POLICY_FULL                   = "GETINFO exit-policy/full";
 }
